e5b3c47785c530e348d9f839cddfba398a6fedfd
Rebeca Cruz <rebeca.cruz@catolica.edu.sv>
2024-11-28 16:59:54 -0600

    segundo

---
+++ src/services/updateUser.js
@@ -0,0 +1,15 @@
+const UpdateUserService = async (userId, formData) => {
+    try {
+        const response = await fetch(`${import.meta.env.VITE_API_URL}/users-update/${userId}`, {
+            method: 'PUT',
+            body: formData,
+            credentials: 'include', 
+        });
+        return response;
+    } catch (error) {
+        console.error('Error updating user:', error);
+        throw error;
+    }
+};
+
+export default UpdateUserService;
