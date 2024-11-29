59f550de66db14ceb03460e68e8caf5972803499
Rebeca Cruz <rebeca.cruz@catolica.edu.sv>
2024-11-29 14:44:59 -0600

    actualizacion main style y bodyy

--- src/pages/BodyPageRender.js
+++ src/pages/BodyPageRender.js
@@ -173,53 +173,60 @@ const RenderBodypage = async() => {
     
     
 
-    <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-purple-100 border-r border-purple-300 sm:translate-x-0 dark:bg-purple-900 dark:border-purple-700" aria-label="Sidebar">
-      <div class="h-full px-3 pb-4 overflow-y-auto bg-purple-100 dark:bg-purple-900">
-         <ul class="space-y-2 font-medium">
+    <aside id="logo-sidebar" class="fixed top-0 left-0 z-40 w-64 h-screen pt-20 transition-transform -translate-x-full bg-white border-r border-gray-200 sm:translate-x-0 dark:bg-gray-800 dark:border-gray-700" aria-label="Sidebar">
+    <div class="h-full px-3 pb-4 overflow-y-auto bg-white dark:bg-gray-800">
+        <ul class="space-y-2 font-medium">
             <li>
-               <a href="#" class="flex items-center p-2 text-purple-900 rounded-lg dark:text-purple-100 hover:bg-purple-200 dark:hover:bg-purple-800 group">
-                  <svg class="w-5 h-5 text-purple-500 transition duration-75 dark:text-purple-300 group-hover:text-purple-900 dark:group-hover:text-purple-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
-                     <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
-                     <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
-                  </svg>
-                  <span class="ms-3">Dashboard</span>
-               </a>
+                <a href="#" class="flex items-center p-2 text-purple-800 rounded-lg dark:text-purple-100 hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <svg class="w-5 h-5 text-purple-600 transition duration-75 dark:text-purple-400 group-hover:text-purple-900 dark:group-hover:text-purple-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 22 21">
+                    <path d="M16.975 11H10V4.025a1 1 0 0 0-1.066-.998 8.5 8.5 0 1 0 9.039 9.039.999.999 0 0 0-1-1.066h.002Z"/>
+                    <path d="M12.5 0c-.157 0-.311.01-.565.027A1 1 0 0 0 11 1.02V10h8.975a1 1 0 0 0 1-.935c.013-.188.028-.374.028-.565A8.51 8.51 0 0 0 12.5 0Z"/>
+                </svg>
+                <span class="ms-3">Dashboard</span>
+                </a>
             </li>
             <li>
-               <a href="#" class="flex items-center p-2 text-purple-900 rounded-lg dark:text-purple-100 hover:bg-purple-200 dark:hover:bg-purple-800 group">
-                  <svg class="w-6 h-6 text-purple-900 dark:text-purple-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
-                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v14m-8-7h2m0 0h2m-2 0v2m0-2v-2m12 1h-6m6 4h-6M4 19h16c.5523 0 1-.4477 1-1V6c0-.55228-.4477-1-1-1H4c-.55228 0-1 .44772-1 1v12c0 .5523.44772 1 1 1Z"/>
-                  </svg>
-                  <span class="ms-3">Crear Producto</span>
-               </a>
+                <a href="#" class="flex items-center p-2 text-purple-800 rounded-lg dark:text-purple-100 hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <svg class="w-6 h-6 text-purple-600 dark:text-purple-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" width="24" height="24" fill="none" viewBox="0 0 24 24">
+                    <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 5v14m-8-7h2m0 0h2m-2 0v2m0-2v-2m12 1h-6m6 4h-6M4 19h16c.5523 0 1-.4477 1-1V6c0-.55228-.4477-1-1-1H4c-.55228 0-1 .44772-1 1v12c0 .5523.44772 1 1 1Z"/>
+                </svg>
+                <!-- Modal toggle -->
+                <div data-modal-target="default-modal" data-modal-toggle="default-modal" class="block text-purple-800 dark:text-purple-100 focus:ring-4 focus:outline-none focus:ring-purple-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center">
+                    Crear Producto
+                </div>
+                </a>
             </li>
             <li>
-               <a href="#" class="flex items-center p-2 text-purple-900 rounded-lg dark:text-purple-100 hover:bg-purple-200 dark:hover:bg-purple-800 group">
-                  <div id="theme-toggle" type="button" class="text-purple-900 dark:text-purple-100 hover:bg-purple-200 dark:hover:bg-purple-800 focus:outline-none focus:ring-4 focus:ring-purple-300 dark:focus:ring-purple-700 rounded-lg text-2sm p-2.5">
-                     <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586a.999.999 0 0 0-1-1.066h.002Z"></path></svg>
-                     <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>tema de color
-                  </div>
-               </a>
+                <a href="#" class="flex items-center p-2 text-purple-800 rounded-lg dark:text-purple-100 hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <div id="theme-toggle" type="button" class="text-purple-800 dark:text-purple-100 hover:bg-gray-100 dark:hover:bg-gray-700 focus:outline-none focus:ring-4 focus:ring-purple-200 dark:focus:ring-purple-700 rounded-lg text-2sm p-2.5">
+                    <svg id="theme-toggle-dark-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M17.293 13.293A8 8 0 016.707 2.707a8.001 8.001 0 1010.586 10.586z"></path></svg>
+                    <svg id="theme-toggle-light-icon" class="hidden w-5 h-5" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path d="M10 2a1 1 0 011 1v1a1 1 0 11-2 0V3a1 1 0 011-1zm4 8a4 4 0 11-8 0 4 4 0 018 0zm-.464 4.95l.707.707a1 1 0 001.414-1.414l-.707-.707a1 1 0 00-1.414 1.414zm2.12-10.607a1 1 0 010 1.414l-.706.707a1 1 0 11-1.414-1.414l.707-.707a1 1 0 011.414 0zM17 11a1 1 0 100-2h-1a1 1 0 100 2h1zm-7 4a1 1 0 011 1v1a1 1 0 11-2 0v-1a1 1 0 011-1zM5.05 6.464A1 1 0 106.465 5.05l-.708-.707a1 1 0 00-1.414 1.414l.707.707zm1.414 8.486l-.707.707a1 1 0 01-1.414-1.414l.707-.707a1 1 0 011.414 1.414zM4 11a1 1 0 100-2H3a1 1 0 000 2h1z" fill-rule="evenodd" clip-rule="evenodd"></path></svg>tema de color
+                </div>
+                </a>
             </li>
             <li>
-               <a id="users" class="flex items-center p-2 text-purple-900 rounded-lg dark:text-purple-100 hover:bg-purple-200 dark:hover:bg-purple-800 group">
-                  <svg class="flex-shrink-0 w-5 h-5 text-purple-500 transition duration-75 dark:text-purple-300 group-hover:text-purple-900 dark:group-hover:text-purple-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
-                     <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
-                  </svg>
-                  <span class="ms-3">Ver usuarios</span>
-               </a>
+                <a id="users" class="flex items-center p-2 text-purple-800 rounded-lg dark:text-purple-100 hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <svg class="flex-shrink-0 w-5 h-5 text-purple-600 transition duration-75 dark:text-purple-400 group-hover:text-purple-900 dark:group-hover:text-purple-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 18">
+                    <path d="M14 2a3.963 3.963 0 0 0-1.4.267 6.439 6.439 0 0 1-1.331 6.638A4 4 0 1 0 14 2Zm1 9h-1.264A6.957 6.957 0 0 1 15 15v2a2.97 2.97 0 0 1-.184 1H19a1 1 0 0 0 1-1v-1a5.006 5.006 0 0 0-5-5ZM6.5 9a4.5 4.5 0 1 0 0-9 4.5 4.5 0 0 0 0 9ZM8 10H5a5.006 5.006 0 0 0-5 5v2a1 1 0 0 0 1 1h11a1 1 0 0 0 1-1v-2a5.006 5.006 0 0 0-5-5Z"/>
+                </svg>
+                <div class="block space-y-4 md:flex md:space-y-0 md:space-x-4 rtl:space-x-reverse">
+                    <div data-modal-target="extralarge-modal" data-modal-toggle="extralarge-modal" class="block w-full md:w-auto text-purple-800 dark:text-purple-100 font-medium rounded-lg text-sm px-5 py-2.5 text-center type="button">
+                        ver usuarios
+                    </div>
+                </div>
+                </a>
             </li>
             <li>
-               <a href="#" class="flex items-center p-2 text-purple-900 rounded-lg dark:text-purple-100 hover:bg-purple-200 dark:hover:bg-purple-800 group">
-                  <svg class="flex-shrink-0 w-5 h-5 text-purple-500 transition duration-75 dark:text-purple-300 group-hover:text-purple-900 dark:group-hover:text-purple-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 18 16">
-                     <path stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M1 8h11m0 0L8 4m4 4-4 4m4-11h3a2 2 0 0 1 2 2v10a2 2 0 0 1-2 2h-3"/>
-                  </svg>
-                  <span id="logout" class="ms-3">Salir</span>
-               </a>
+                <a href="#" class="flex items-center p-2 text-purple-800 rounded-lg dark:text-purple-100 hover:bg-gray-100 dark:hover:bg-gray-700 group">
+                <svg class="flex-shrink-0 w-5 h-5 text-purple-600 transition duration-75 dark:text-purple-400 group-hover:text-purple-900 dark:group-hover:text-purple-100" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20">
+                    <path fill-rule="evenodd" d="M10 3a7 7 0 1 0 7 7H3a7 7 0 0 0 7-7ZM2 10a8 8 0 1 1 16 0 8 8 0 0 1-16 0Z" clip-rule="evenodd"/>
+                </svg>
+                <span class="ms-3">Configuración</span>
+                </a>
             </li>
-         </ul>
-      </div>
-   </aside>
+        </ul>
+    </div>
+    </aside>
    
    
 
